f002ab625d2399ad7b32b1f959d05690cf0614b7
Vasily <vaskonov@users.noreply.github.com>
2021-10-13 19:27:11 +0300

    Add files via upload

--- pmi.py
+++ pmi.py
@@ -3,7 +3,7 @@ from composes.transformation.scaling.ppmi_weighting import PpmiWeighting
 from composes.transformation.dim_reduction.svd import Svd
 from composes.matrix.sparse_matrix import SparseMatrix
 from composes.similarity.cos import CosSimilarity
-import pdb
+from collections import Counter
 
 #data = './wiki-word2vec/pmi_no_suf/average-1.0-data'
 #rows = './wiki-word2vec/pmi_no_suf/average-1.0-rows'
@@ -13,16 +13,78 @@ data='/tmp/data.pmi'
 rows='/tmp/rows.pmi'
 cols='/tmp/cols.pmi'
 
-my_space = Space.build(data = data, rows = rows, cols = cols, format = "sm")
-my_space = my_space.apply(PpmiWeighting())
+def build_pairs(post_lemma, ng):
+    print("build_pairs: "+str(ng))
+    omit = ['BEG', 'END', 'UNK']
+    word_context = []
+    words = []
 
-#pdb.set_trace()
+#     print('build_pairs: total articles', len(post_lemma))
+
+    for idx, text in enumerate(post_lemma):
+        
+        text = text.split(" ")
+        
+        for i in range(ng):
+            text = ['BEG'] + text
+            text = text + ['END']
+
+        for idx in range(ng,len(text)-ng):
+            for i in range(idx-ng, idx):
+                if text[i] not in omit:
+                    word_context.append((text[idx], text[i]))
+                
+            for i in range(idx+1, idx+ng+1):
+                if text[i] not in omit:
+                    word_context.append((text[idx], text[i]))
+
+#     print('build_pairs: ng:', ng, ' num of pairs:', len(word_context))
+    return dict(Counter(word_context))
 
 
-my_space = my_space.apply(Svd(200))
+def convert(fin,fout):
+    x = []
+    
+    with open(fin) as f:
+        for l in f:
+            x.append(l.strip().split("\t"))
+    
+    with open(fout, 'w') as f:        
+        f.write(str(len(x))+ " "+ str(len(x[0][1:]))+"\n")
+        for l in x:
+            f.write(" ".join(l)+"\n")
 
 
-my_space.export("/tmp/space", format='dm')
+def prepare_pmi(pmi):
+    print("prepare_pmi")
+    rows = [i[0] for i in list(pmi.keys())]
+    cols = [i[1] for i in list(pmi.keys())]
+    
+    with open('/tmp/data.pmi', 'w') as f:
+        for v in zip(list(pmi.keys()), list(pmi.values())):
+            f.write(" ".join(v[0])+" "+str(v[1])+"\n")
+        
+    with open('/tmp/rows.pmi', 'w') as f:
+        for r in rows:
+            f.write(r+"\n")
+            
+    with open('/tmp/cols.pmi', 'w') as f:
+        for c in cols:
+            f.write(c+"\n")
+
+            
+def pmi(dim=0):
+    print("pmi: "+str(dim))
+    my_space = Space.build(data = data, rows = rows, cols = cols, format = "sm")
+#     Positive Point-wise Mutual Information
+    my_space = my_space.apply(PpmiWeighting())
+
+    if dim!=0:
+        my_space = my_space.apply(Svd(dim))
+
+
+    my_space.export("/tmp/space", format='dm')
+    return True
 #from sklearn.decomposition import TruncatedSVD
 
 #def svd(space, dim):
@@ -34,6 +96,6 @@ my_space.export("/tmp/space", format='dm')
 # print my_space.cooccurrence_matrix
 # print my_space.id2column
 
-pdb.set_trace()
+#pdb.set_trace()
 #get_sim(key, value, CosSimilarity())
 #space.id2row
--- tools.py
+++ tools.py
@@ -1,78 +1,55 @@
-import nltk
 import json
 import torch
+import numpy as np
 from torch import nn
-from nltk import word_tokenize
-from nltk.util import ngrams
 from gensim.models import KeyedVectors
+from gensim.corpora.wikicorpus import WikiCorpus
+from gensim.models import FastText
+from gensim.models.word2vec import Word2Vec
 from collections import OrderedDict
-from collections import Counter
+import multiprocessing
+
 
 # # based on text build windows n pairs of (word,context)
 # def count_pairs(text, num):
 #     ngs = list(ngrams(text.split(), num))
 #     return dict(Counter(ngs))
 
-def build_pairs(post_lemma, ng):
-    omit = ['BEG', 'END', 'UNK']
-    word_context = []
-    words = []
+def w2v(vector_size, window, min_count, fname, fcorpus, sg=1):
+    params = {'sg':sg, 
+          'vector_size': vector_size, 
+          'window': window, 
+          'min_count': min_count, 
+          'sample': 1e-3, 
+          'workers': max(1, multiprocessing.cpu_count() - 1)}
+    word2vec = Word2Vec(corpus_file=fcorpus, **params)
+    word2vec.wv.save_word2vec_format(fname)
+    return True
 
-#     print('build_pairs: total articles', len(post_lemma))
 
-    for idx, text in enumerate(post_lemma):
-        
-        text = text.split(" ")
-        
-        for i in range(ng):
-            text = ['BEG'] + text
-            text = text + ['END']
+def ft(vector_size, window, min_count, fname, fcorpus):
+    ft = FastText(corpus_file=fcorpus, 
+              vector_size = vector_size, 
+              window = window, 
+              min_count = min_count, 
+              epochs=10)
+    ft.wv.save_word2vec_format(fname)
+    return True
 
-        for idx in range(ng,len(text)-ng):
-            for i in range(idx-ng, idx):
-                if text[i] not in omit:
-                    word_context.append((text[idx], text[i]))
-                
-            for i in range(idx+1, idx+ng+1):
-                if text[i] not in omit:
-                    word_context.append((text[idx], text[i]))
 
-#     print('build_pairs: ng:', ng, ' num of pairs:', len(word_context))
-    return dict(Counter(word_context))
+def wiki2text(wiki_f, txt_f):
+    wiki = WikiCorpus(wiki_f, token_min_len=0, lower=True)
+    print("number of articles:", len(wiki))
+    with open(txt_f, 'w') as output:
+        for text in wiki.get_texts():
+            output.write(" ".join(text) + "\n")
+
 
 # # assert(build_pairs(['I like apples and alcohol'],2), )
 # # print(build_pairs(['I like apples and alcohol is this OK'],3))
 # assert(build_pairs(['I like apples and alcohol'],1), [('I', 'like'), ('like', 'I'), ('like', 'apples'), ('apples', 'like'), ('apples', 'and'), ('and', 'apples'), ('and', 'alcohol'), ('alcohol', 'and')])
 
-
-def convert(fin,fout):
-    x = []
-    
-    with open(fin) as f:
-        for l in f:
-            x.append(l.strip().split("\t"))
-    
-    with open(fout, 'w') as f:        
-        f.write(str(len(x))+ " "+ str(len(x[0][1:]))+"\n")
-        for l in x:
-            f.write(" ".join(l)+"\n")
-            
-def prepare_pmi(pmi):
-    rows = [i[0] for i in list(pmi.keys())]
-    cols = [i[1] for i in list(pmi.keys())]
-    
-    with open('/tmp/data.pmi', 'w') as f:
-        for v in zip(list(pmi.keys()), list(pmi.values())):
-            f.write(" ".join(v[0])+" "+str(v[1])+"\n")
-        
-    with open('/tmp/rows.pmi', 'w') as f:
-        for r in rows:
-            f.write(r+"\n")
-            
-    with open('/tmp/cols.pmi', 'w') as f:
-        for c in cols:
-            f.write(c+"\n")
-            
+           
 def score(test_f, model_f, vocab=[]):
     with open(test_f, 'r') as f:
         test_d = json.load(f)   
@@ -89,8 +66,9 @@ def score(test_f, model_f, vocab=[]):
                 if value not in vocab or key not in vocab:
                     continue
             else:
-                if key in we.index_to_key and value in we.index_to_key:
-                    scores[(key, value)] = (we[key], we[value], 1)
+                if key not in we.index_to_key or value not in we.index_to_key:
+                    continue
+            scores[(key, value)] = (we[key], we[value], 1)
 
     eval_values = list(test_cd.values())
     for idx, key in enumerate(list(test_cd.keys())):
@@ -101,9 +79,10 @@ def score(test_f, model_f, vocab=[]):
                     if value not in vocab or key not in vocab:
                         continue
                 else:
-                    if key in we.index_to_key and value in we.index_to_key:
-                        scores[(key, value)] = (we[key], we[value], 0)
-
+                    if key not in we.index_to_key or value not in we.index_to_key:
+                        continue
+                
+                scores[(key, value)] = (we[key], we[value], 0)
     return scores
     
 def loss(scores):
@@ -111,16 +90,21 @@ def loss(scores):
     x = [i[0] for i in scores.values()]
     y = [i[1] for i in scores.values()]
     target = [i[2] for i in scores.values()]
-    return cos_loss(torch.tensor(x, dtype=torch.float64),torch.tensor(y, dtype=torch.float64), torch.tensor(target))
+    
+    return cos_loss(torch.tensor(x, dtype=torch.float64),
+                    torch.tensor(y, dtype=torch.float64), 
+                    torch.tensor(target))
 
     
 def get_vocab(wes_fs):
     vocabs = []
     for wes_f in wes_fs:
-        vocabs.append(KeyedVectors.load_word2vec_format(wes_f, binary=False).index_to_key)
+        vocab = KeyedVectors.load_word2vec_format(wes_f, binary=False).index_to_key
+        vocabs.append(vocab)
+        print(wes_f, len(vocab))
         
     joint_vocab = vocabs[0]
     for vocab in vocabs:
-        joint_vocab = numpy.intersect1d(joint_vocab, vocab).tolist()
+        joint_vocab = np.intersect1d(joint_vocab, vocab).tolist()
         
     return joint_vocab
