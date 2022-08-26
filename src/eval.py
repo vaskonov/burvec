3ae889264d8fde21763117e77a3fe20be752c0d8
Vasily <vaskonov@users.noreply.github.com>
2022-08-26 11:37:26 +0300

    Add files via upload

--- src/eval.py
+++ src/eval.py
@@ -1,11 +1,13 @@
 import json
 import torch
 
+import random
 import numpy as np
 from torch import nn
 
 from collections import OrderedDict
 from gensim.models import KeyedVectors
+from sklearn.metrics.pairwise import cosine_similarity
 
 def loss(scores):
     cos_loss = nn.CosineEmbeddingLoss()
@@ -33,7 +35,18 @@ def loss(scores):
 def get_vocab(wes_fs):
     vocabs = []
     for wes_f in wes_fs:
-        vocab = KeyedVectors.load_word2vec_format(wes_f, binary=False).index_to_key
+        
+        if str(wes_f).split("/")[-1] in ['pmiWeighting', 'PlmiWeighting', 'PlogWeighting', 'PpmiWeighting', 'EpmiWeighting', 'ft']:
+            continue
+        
+        print(wes_f)
+        try:
+            vocab = KeyedVectors.load_word2vec_format(wes_f, binary=False).index_to_key
+        except:
+            vocab = KeyedVectors.load_word2vec_format(wes_f, binary=False, no_header=True).index_to_key
+
+        vocab = [x for x in vocab if x is not None]
+        
         vocabs.append(vocab)
         print(wes_f, len(vocab))
     
@@ -52,39 +65,38 @@ def score(test_f, model_f, vocab=[]):
     we = KeyedVectors.load_word2vec_format(model_f, binary=False)
     test_cd = OrderedDict(test_d)
     
-    scores = {}
+    total = []
+    oov = []
     
-    for key, values in test_cd.items():
-        for value in values:
-            
-            if len(vocab)>0:
-                if value not in vocab or key not in vocab:
-                    continue
-            else:
-                if key not in we.index_to_key or value not in we.index_to_key:
-                    continue
-            scores[(key, value)] = (we[key], we[value], 1)
+    positive = {}
+    negative = {}
+    
+    for values in list(test_cd.values()):
+        for idx, value1 in enumerate(values[:-1]):
+            for value2 in values[idx+1:]:
+                if value1 in vocab and value2 in vocab:
+                    positive[(value1, value2)] = (we[value1], we[value2], 1)
 
     eval_values = list(test_cd.values())
-    for idx, key in enumerate(list(test_cd.keys())):
-        if idx+1<len(eval_values):
-            for value in eval_values[idx+1]:
-                
-                if len(vocab)>0:
-                    if value not in vocab or key not in vocab:
-                        continue
-                else:
-                    if key not in we.index_to_key or value not in we.index_to_key:
-                        continue
-                
-                scores[(key, value)] = (we[key], we[value], -1)
-    return scores
+    for idx, values1 in enumerate(eval_values[:-2]):
+        for value2 in eval_values[idx+2]:
+            for value1 in values1:
+                if value1 in vocab and value2 in vocab:
+                    if len(negative.keys()) < len(positive.keys()):
+                        negative[(value1, value2)] = (we[value1], we[value2], -1)
+        
+    print('total words',len(list(set(total))))
+    print('oov words', len(list(set(oov))))
+    print('positive pairs', len([a for a in list(positive.values()) if a[2] == 1]))
+    print('negative pairs', len([a for a in list(negative.values()) if a[2] == -1]))
+          
+    return {**positive, **negative}
 
 
 def tabular(scores_d, language):
     for method, scores in scores_d[language].items():
         print(method)
         for vector_size, windows in scores.items():
-            ws = [str(round(a,3)) for a in list(windows.values())]
+            ws = [str(round(a,3)) for a in list(windows.values()) if type(a) is not list]
             print("&",vector_size," & "," & ".join(ws),"\\\\")
     return True
